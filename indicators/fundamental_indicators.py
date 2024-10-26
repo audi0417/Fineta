991898c206e3d615bd231eb51878da7d0334bed2
audi0417 <64790460+audi0417@users.noreply.github.com>
2024-10-27 00:02:00 +0800

    Update fundamental_indicators.py

--- indicators/fundamental_indicators.py
+++ indicators/fundamental_indicators.py
@@ -12,11 +12,5 @@ def calculate_fundamentals(df: pd.DataFrame) -> pd.DataFrame:
         'Dividend Yield': []
     }
 
-    for stock in df.index.levels[0]:
-        fundamental_data['Stock'].append(stock)
-        fundamental_data['EPS'].append(np.random.uniform(1, 10))
-        fundamental_data['P/E'].append(np.random.uniform(10, 30))
-        fundamental_data['P/B'].append(np.random.uniform(1, 5))
-        fundamental_data['Dividend Yield'].append(np.random.uniform(1, 5))
 
     return pd.DataFrame(fundamental_data)
