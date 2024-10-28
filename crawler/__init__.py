b3175872fa63e2ce6ce0cbe29f3ed8ebc3fa66e2
audi0417 <64790460+audi0417@users.noreply.github.com>
2024-10-28 16:06:30 +0800

    Update __init__.py
    *修正bug

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,5 +1,7 @@
-from .financial_report import FinancialScraper
+from .financial_report import FinancialReportScraper
 from .stock_price_fetcher import StockPriceFetcher
+from .esg_report import ESGReportScraper
 from .exceptions import FinancialScraperError
 
-__all__ = ['FinancialScraper', 'FinancialScraperError','StockPriceFetcher']
+
+__all__ = ['FinancialReportScraper', 'FinancialScraperError','ESGReportScraper','StockPriceFetcher']
