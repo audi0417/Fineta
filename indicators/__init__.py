2a38f51af1abcccf06a21a80b37c596ad038bd02
audi0417 <64790460+audi0417@users.noreply.github.com>
2024-10-30 00:26:58 +0800

    Update __init__.py
    *新增回報的計算

--- indicators/__init__.py
+++ indicators/__init__.py
@@ -1,5 +1,6 @@
 from .fundamental_indicators import FundamentalIndicators
 from .risk_indicators import RiskIndicators
 from .technical_indicators import TechnicalIndicators
+from .return_indicators import ReturnIndicators
 
-__all__ = ['FundamentalIndicators', 'RiskIndicators','TechnicalIndicators']
+__all__ = ['FundamentalIndicators', 'RiskIndicators','TechnicalIndicators','ReturnIndicators']
